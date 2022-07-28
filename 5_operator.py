c50cb8794bb42e4ba0d5d11a635fa078893c738b
Al-tsao <al.tsao.yw@gmail.com>
2022-07-28 12:47:52 +0200

    revise

--- 5_operator.py
+++ 5_operator.py
@@ -1,5 +1,5 @@
 #算數運算子
-x = 5
+""" x = 5
 y = 3
 print(x + y)
 print(x - y)
@@ -8,10 +8,22 @@ print(x / y)
 print(x // y) #取整數，不會四捨五入
 print(x ** y) #次方
 print(x % y) #mod: 取餘數
-print(x ** (1/y)) #開y次方
-
+print(x ** (1/y)) #開y次方 """
 
 #餘數應用
+""" n = int(input())
+print(f"百位數字:{n // 100}")
+print(f"十位數字:{(n // 10)%10}")
+print(f"百位數字:{n % 10}") """
+
+#指定運算子
+
+#交換數值
+x = int(input("X: "))
+y = int(input("y: "))
+temp = x
+x = y
+y = temp
 
 
 
