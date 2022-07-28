0490655f19bffb6af9fc5327a01e2b430208ea41
Al-tsao <al.tsao.yw@gmail.com>
2022-07-28 12:35:18 +0200

    revise

---
+++ 5_operator.py
@@ -0,0 +1,20 @@
+#算數運算子
+x = 5
+y = 3
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y) #取整數，不會四捨五入
+print(x ** y) #次方
+print(x % y) #mod: 取餘數
+print(x ** (1/y)) #開y次方
+
+
+#餘數應用
+
+
+
+
+
+
