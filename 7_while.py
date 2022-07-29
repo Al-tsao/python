f56c4de4d0af3471548b24835880667636ab9fcf
Al-tsao <al.tsao.yw@gmail.com>
2022-07-29 10:31:50 +0200

    revise

--- 7_while.py
+++ 7_while.py
@@ -12,9 +12,16 @@ while True:
     print(x) """
 
 # break V.S. continue
+""" x = 0
+while x <= 10:
+    x += 1
+    if x == 3:
+        break # 直接跳出整個回圈
+    print(x) """
+
 x = 0
 while x <= 10:
     x += 1
     if x == 3:
-        break
+        continue  # 終止以下的所有程式碼，直接進行下一個迴圈
     print(x)
