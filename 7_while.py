7e527853fb9dc4b0eff14fc751611e829041d426
Al-tsao <al.tsao.yw@gmail.com>
2022-07-29 10:29:50 +0200

    revise

--- 7_while.py
+++ 7_while.py
@@ -3,10 +3,18 @@ while i < 6:
     print(i)
     i += 1 """
 
-#while + break
-i = 0
+# while + break
+""" i = 0
 while True:
     x = input()
     if x == 'Q':
+        break
+    print(x) """
+
+# break V.S. continue
+x = 0
+while x <= 10:
+    x += 1
+    if x == 3:
         break
     print(x)
