92dd4e5d9a1cfb0ca97efe58cc8ac67d16abfb9c
Al-tsao <al.tsao.yw@gmail.com>
2022-08-03 15:32:31 +0200

    revise

--- 12_nested_loop.py
+++ 12_nested_loop.py
@@ -1,3 +1,13 @@
 for i in range(3):
     for j in range(2):
         print(i, j)
+
+#30:06
+
+n = int(input())
+for i in range(n):
+    for k in range(n - i - 1):
+        print(" ", end="")
+    for j in rnage(i + 1):
+        print("*", end="")
+    print()
