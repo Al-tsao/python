ff30bddf9ccbd2e3e7fa119c7119a14f90d24017
Al-tsao <al.tsao.yw@gmail.com>
2022-08-04 22:00:49 +0200

    revise

--- 14_lambda.py
+++ 14_lambda.py
@@ -1,5 +1,5 @@
 #匿名函數
-def f(x):
+""" def f(x):
     return x + 3
 print(f(3))
 
@@ -7,3 +7,11 @@ add = lambda x: x+3
 print(add(3))
 
 print((lambda x: x+3)(3))
+ """
+#lambda的組合
+#和filter
+numbers = [3, 50, 2, 80, 49, 10, 6]
+print(list(filter(lambda x: x > 10, numbers)))
+#和map
+scores = input().split()
+map(lambda x: int(x), scores)
